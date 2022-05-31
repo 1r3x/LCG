9ff00ac0b2b65b7b50c085c168b90ec1eef32006
1r3x <akib.hosain@live.com>
2022-06-01 00:02:07 +0600

    preSchedulePayment done.

---
+++ DataAccessLibrary/Implementation/GetPreSchedulePaymentInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Interfaces;
+using EntityModelLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace DataAccessLibrary.Implementation
+{
+    public class GetPreSchedulePaymentInfo : IGetPreSchedulePaymentInfo
+    {
+        private readonly DbContextForTest _dbContext;
+        private readonly DbContextForProdOld _dbContextProdOld;
+        public GetPreSchedulePaymentInfo(DbContextForTest dbContext, DbContextForProdOld dbContextProdOld)
+        {
+            _dbContext = dbContext;
+            _dbContextProdOld = dbContextProdOld;
+        }
+        public async Task<IList<LcgPaymentSchedule>> GetAllPreSchedulePaymentInfo(string environment)
+        {
+            //todo after quality check should  active the code block
+            //DateTime startDate;
+            //DateTime endDate;
+            //if (DateAndTime.Now.DayOfWeek.ToString() == "Monday")
+            //{
+            //    startDate = DateTime.Now.AddDays(-1);
+            //    endDate = DateTime.Now;
+            //}
+            //else
+            //{
+            //    startDate = DateTime.Now;
+            //    endDate = DateTime.Now;
+            //}
+            //if (environment == "T")
+            //{
+            //    return await _dbContext.LcgPaymentSchedules.
+            //        Where(x => x.EffectiveDate >= startDate && x.EffectiveDate <= endDate).ToListAsync();
+            //}
+            //else if (environment=="PO")
+            //{
+            //    return await _dbContextProdOld.LcgPaymentSchedules.
+            //        Where(x => x.EffectiveDate >= startDate && x.EffectiveDate <= endDate).ToListAsync();
+            //}
+            //else
+            //{
+            //    return await _dbContext.LcgPaymentSchedules.
+            //        Where(x => x.EffectiveDate >= startDate && x.EffectiveDate <= endDate).ToListAsync(); ;
+            //}
+
+            return await _dbContext.LcgPaymentSchedules.ToListAsync();
+        }
+    }
+}
---
+++ LCG/Pages/PreSchedulePosting/PreSchedulePosting.razor.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ApiAccessLibrary.ApiModels;
+using ApiAccessLibrary.Interfaces;
+using DataAccessLibrary.Implementation;
+using DataAccessLibrary.Interfaces;
+using EntityModelLibrary.Models;
+using LCG.Data;
+using LCG.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace LCG.Pages.PreSchedulePosting
+{
+    public partial class PreSchedulePosting
+    {
+        [Inject] private IGetPreSchedulePaymentInfo GetPreSchedulePaymentInfo { get; set; }
+        [Inject] private IGetDetailsOfPreSchedulePayment GetDetailsOfPreSchedulePayment { get; set; }
+        [Inject] private IPopulateDataForProcessSales PopulateData { get; set; }
+        [Inject] private IAddCcPayment AddCcPayment { get; set; }
+        [Inject] private DbContextForTest DbContext { get; set; }
+        [Inject] private DbContextForProdOld DbContextProdOld { get; set; }
+        [Inject] private IProcessSaleTransactions SaleApi { get; set; }
+        [Inject] private IAddNotes AddNotes { get; set; }
+
+        private IList<LcgPaymentSchedule> _paymentSchedule;
+        private LcgTablesViewModel _preScheduleLcgTablesViewModel;
+
+
+        private ViewSaleResponseModel _responseModel;
+        private readonly DateTime _scheduleDateTime = DateTime.Now;
+        private string _errorModel;
+        private decimal _tempAmount;
+        private int _loadingBar;
+        private bool _busyClick;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _paymentSchedule = await GetPreSchedulePaymentInfo.GetAllPreSchedulePaymentInfo("T");
+            _loadingBar = 0;
+
+        }
+
+        async Task OpenOrder(int orderId)
+        {
+            _responseModel = null;
+            _errorModel = null;
+            _busyClick = true;
+            _loadingBar = 1;
+            _preScheduleLcgTablesViewModel =
+                await GetDetailsOfPreSchedulePayment.GetDetailsOfPreSchedulePaymentInfo(orderId, "T");
+            _tempAmount = _preScheduleLcgTablesViewModel.Amount;
+            await ProcessSaleTrans();
+            _loadingBar = 0;
+            _paymentSchedule = await GetPreSchedulePaymentInfo.GetAllPreSchedulePaymentInfo("T");
+            _busyClick = false;
+        }
+        private async Task ProcessSaleTrans()
+        {
+            _responseModel = null;
+            _errorModel = null;
+            var saleRequestModel = new SaleRequestModel()
+            {
+                Outlet = new ApiAccessLibrary.ApiModels.Outlet()
+                {
+                    MerchantID = "192837645",
+                    StoreID = "0001",
+                    TerminalID = "0001"
+                },
+                Amount = _preScheduleLcgTablesViewModel.Amount,
+                PaymentMethod = "OnFile",
+                PaymentMethodID = _preScheduleLcgTablesViewModel.PaymentMethodId,
+                Patient = new ApiAccessLibrary.ApiModels.Patient()
+                {
+                    AccountNumber = _preScheduleLcgTablesViewModel.PatientAccount
+                }
+            };
+            try
+            {
+                var resultVerify = await SaleApi.PostProcessSalesTransactionAsync(saleRequestModel);
+                if (resultVerify.Contains("FieldErrors"))
+                {
+                    _errorModel = resultVerify;
+                }
+                else
+                {
+                    _responseModel = new ViewSaleResponseModel(resultVerify);
+
+                }
+
+                string noteText = null;
+                if (@_responseModel != null && _responseModel.ResponseCode == "000")
+                {
+                    noteText = "INSTAMED CC APPROVED FOR $" + _tempAmount + " " + @_responseModel.ResponseMessage.ToUpper() +
+                                  " AUTH #:" + @_responseModel.AuthorizationNumber;
+                    await SaveCardInfoAndScheduleData();
+
+                }
+                else
+                {
+                    if (@_responseModel != null)
+                        noteText = "INSTAMED CC DECLINED FOR $" + _tempAmount + " " +
+                                   @_responseModel.ResponseMessage.ToUpper() +
+                                   " AUTH #:" + @_responseModel.AuthorizationNumber;
+                }
+                //actual employee =31950
+                //todo debtor account
+                //var debtorAcct = (await PopulateData.GetDebtorAccountNoByPatientAcct(_preScheduleLcgTablesViewModel.PatientAccount, "T")).ToString();
+                //todo remove demo acct
+                await AddNotes.Notes(_preScheduleLcgTablesViewModel.AssociateDebtorAcct, 31950, "RA", noteText, "N", null, "T");//PO for prod_old & T is for test_db
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+
+        private async Task SaveCardInfoAndScheduleData()
+        {
+            try
+            {
+
+                var paymentScheduleExample = new LcgPaymentScheduleHistory()
+                {
+                    ResponseCode = _responseModel.ResponseCode,
+                    AuthorizationNumber = _responseModel.AuthorizationNumber,
+                    AuthorizationText = Environment.UserName,
+                    ResponseMessage = _responseModel.ResponseMessage,
+                    PaymentScheduleId = _preScheduleLcgTablesViewModel.IdForPaymentSchedule,
+                    TransactionId = _responseModel.TransactionId
+                };
+
+                DbContext.LcgPaymentScheduleHistories.Add(paymentScheduleExample);
+                await DbContext.SaveChangesAsync();
+
+                var paymentScheduleUpdate =
+                    DbContext.LcgPaymentSchedules.FirstAsync(x => x.Id == _preScheduleLcgTablesViewModel.IdForPaymentSchedule);
+                paymentScheduleUpdate.Result.IsActive = false;
+                await DbContext.SaveChangesAsync();
+
+                var ccPaymentObj = new CcPayment()
+                {
+                    DebtorAcct = _preScheduleLcgTablesViewModel.PatientAccount,
+                    Company = "TOTAL CREDIT RECOVERY",
+                    //UserId = Environment.UserName,
+                    //UserName = Environment.UserName + " -LCG",
+                    UserId = "LCG",
+                    UserName = "PD -LCG",
+                    ChargeTotal = _preScheduleLcgTablesViewModel.Amount,
+                    Subtotal = _preScheduleLcgTablesViewModel.Amount,
+                    PaymentDate = _scheduleDateTime,
+                    ApprovalStatus = "APPROVED",
+                    //todo card holder it must be saved from multiple page
+                    BillingName = _preScheduleLcgTablesViewModel.CardHolderName,
+                    ApprovalCode = _responseModel.ResponseCode,
+                    OrderNumber = _responseModel.TransactionId,
+                    RefNumber = "INSTAMEDLH",
+                    Sif = "N"
+                };
+                await AddCcPayment.CreateCcPayment(ccPaymentObj, "T");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }
+
+
+}
